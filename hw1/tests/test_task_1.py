a89e3f1f7bc911f6523537f8da87fd12890a7673
brevnishko2 <73742561+brevnishko2@users.noreply.github.com>
2020-11-12 17:05:33 +0300

    homework 2

---
+++ hw1/tests/test_task_1.py
@@ -0,0 +1,13 @@
+import pytest
+
+from hw1.tasks.task_1 import check_power_of_2
+
+
+@pytest.mark.parametrize(
+    ["value", "expected_result"],
+    [(65536, True), (12, False), (-4, False), (0, False)],
+)
+def test_power_of_2(value: int, expected_result: bool):
+    actual_result = check_power_of_2(value)
+
+    assert actual_result == expected_result
