2b5b714a2fe002352bcae02c4ae19319e4682897
brevnishko2 <73742561+brevnishko2@users.noreply.github.com>
2020-12-28 07:58:29 +0300

    fix: change relation and table's name

--- hw12/task/models.py
+++ hw12/task/models.py
@@ -2,33 +2,33 @@ from django.db import models
 
 
 # Create your models here.
-class TeachersTable(models.Model):
+class Teachers(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
 
 
-class StudentsTable(models.Model):
+class Students(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
 
 
-class HomeworkTable(models.Model):
+class Homework(models.Model):
     text = models.TextField()
     deadline_days = models.IntegerField()
     created = models.DateTimeField()
     create_by = models.CharField(max_length=200, default="Unknown Teacher")
 
 
-class HomeworkResultTable(models.Model):
-    homework = models.OneToOneField(HomeworkTable, on_delete=models.CASCADE)
+class HomeworkResult(models.Model):
+    homework = models.CharField(max_length=200)
     solution = models.TextField()
-    author = models.CharField(max_length=200)
+    author = models.OneToOneField(Students, on_delete=models.CASCADE)
     created = models.DateTimeField()
 
 
 class HomeworksDone(models.Model):
     homework = models.OneToOneField(
-        HomeworkTable, on_delete=models.CASCADE, primary_key=True
+        Homework, on_delete=models.CASCADE, primary_key=True
     )
     done_by = models.TextField()
     create_by = models.CharField(max_length=200)
